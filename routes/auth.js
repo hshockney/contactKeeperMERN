849b5266b18b13c36b931190db8308caaef0668c
hshockney <shockney@atlanticbb.net>
2019-07-12 10:59:49 -0400

    Auth routes are finished

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,21 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
 //@route GET api/auth
 //@desc Get logged in user
 // @access Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //@route Post api/auth
@@ -19,7 +26,7 @@ router.get('/', (req, res) => {
 router.post(
   '/',
   [
-    check('email', 'Please include a vlid email').isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
     check('password', 'Password is required').exists()
   ],
   async (req, res) => {
