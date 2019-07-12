20a9e272907620a1ea8207be69d6e48243ab2a36
hshockney <shockney@atlanticbb.net>
2019-07-12 12:38:36 -0400

    Added syntax for adding contact to route

--- routes/contacts.js
+++ routes/contacts.js
@@ -22,21 +22,47 @@ router.get('/', auth, async (req, res) => {
 //@route Post api/contacts
 //@desc  Add new contacts
 // @access Private
-router.post('/', (req, res) => {
-  res.send('Get all contacts');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 //@route Put api/contacts/:id
 //@desc  Update contact
 // @access Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
-});
+router.put('/:id', (req, res) => {});
 
 //@route Delete api/contacts/:id
 //@desc  Delete contact
 // @access Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
-});
+router.delete('/:id', (req, res) => {});
 module.exports = router;
