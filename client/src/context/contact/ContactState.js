be157313f3dc94a933ccd6f8cb64df6cc49525be
hshockney <shockney@atlanticbb.net>
2019-07-15 13:52:04 -0400

    Edit and update contact action implemented

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -59,6 +59,9 @@ const ContactState = props => {
     dispatch({ type: CLEAR_CURRENT });
   };
   // Update Contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   // Filter Contacts
   // Clear Filter
   return (
@@ -69,7 +72,8 @@ const ContactState = props => {
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}
     >
       {props.children}
