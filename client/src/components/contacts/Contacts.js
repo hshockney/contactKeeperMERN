1d74db7798fe460f80ce824d3c6c82463bd91163
hshockney <shockney@atlanticbb.net>
2019-07-15 15:14:46 -0400

    Add css transition to add and delete contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from 'react';
 import ContactContext from '../../context/contact/contactContexts';
 import ContactItem from './ContactItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
   const { contacts, filtered } = contactContext;
@@ -9,13 +10,19 @@ const Contacts = () => {
   }
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map(contact => (
-            <ContactItem contact={contact} key={contact.id} />
-          ))
-        : contacts.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
