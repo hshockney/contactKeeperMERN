3546779e8700c577e01d0e7b586634ecafa87e4d
hshockney <shockney@atlanticbb.net>
2019-07-16 15:05:28 -0400

    Changed the way envs were handled

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ module.exports = function(req, res, next) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
   } catch (error) {
